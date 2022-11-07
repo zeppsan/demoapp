fb957bcb3004a9c14d2de1917ce3e1c77ea5f537
Eric Qvarnström <eric.qvarnstrom@enfogroup.com>
2022-11-07 14:43:56 +0100

    asd

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000
 
 app.use('/', (req, res) => {
     res.send('Welcome to my website!');
 });
 
-app.listen(process.env.NODE_ENV)
+app.listen(port)
